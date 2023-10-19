d17babe4c94605453dc159b7613281fb970c95d4
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-10-19 01:15:46 -0400

    Added _health endpoint for checking if server is responsive

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
+
+// Default port is 8081 unless specified
 const port = process.env.PORT ? +process.env.PORT : 8081;
 
+// Return message and port number
 app.get("/", (req, res) => {
   console.log(req);
-  res.send("Server is up and running.");
+  res.send(`Backend server is up and running. Port ${port}.`);
 });
 
+// Endpoint for health checking
+// Returns 200 status code if server is responsive
+app.get("/_health", (req, res) => {
+  res.status(200).send("");
+});
+
+// Listen on port
 app.listen(port, () => {
   console.log(`Backend server is listening on port ${port}...`);
 });
